ad4eba4d051f0e9553a8aa7f6bd4eca6e16e5321
Oscar Leche <oscar.leche@xoom.com>
2015-05-11 16:23:39 -0600

    Login optimization

--- post/login.php
+++ post/login.php
@@ -1,20 +1,20 @@
 <?php
-	
-	
+
+
 	$user = "13a3b20fa41aa80636cffe064abd3e07";
 	$pass = "ac2097a20d8b989d1b4305bfdc170b4d";
-	
+
 	$fields = array(
 		'username' => $_POST['username'],
 		'password' => $_POST['password'],
 		'scopes' => 'site-visitor'
 	);
-	
+
 	//url-ify the data for the POST
 	$fields_string = "";
 	foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
 	rtrim($fields_string, '&');
-	
+
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, "http://crea.arduinogt.com/v1/session");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -23,25 +23,24 @@
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($ch, CURLOPT_UNRESTRICTED_AUTH, 1);
-	curl_setopt($ch,CURLOPT_POST, count($fields));
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,1)
+	curl_setopt($ch, CURLOPT_POST, count($fields));
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-	
+
 	$output = curl_exec($ch);
 	curl_close($ch);
-	
-	print_r($output);	
 
 	$response = json_decode($output);
-	
+
 	if ($response->code == 200){
 		session_start();
 		$_SESSION['token'] = $response->access_token;
 		$_SESSION['username'] = $_POST['username'];
-		
+
 		header("Location: ../dashboard.php");
 	}else{
 		header("Location: ../login.php?msg=$response->message");
 	}
-	
+
 
 ?>
