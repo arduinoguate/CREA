de9c4767a39da5ce6a0607ccd6dd7e9208b5e29d
Oscar Leche <oleche@donomar.local>
2015-05-15 22:29:17 -0600

    Documentation header logo

--- docs.php
+++ docs.php
@@ -44,13 +44,14 @@
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header page-scroll">
+								<img src="img/crea.png" width="25px">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html"><img src="img/crea.png" width="25px">Crea</a>
+                <a class="navbar-brand" href="index.html">Crea</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
