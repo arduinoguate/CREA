c76a5bba52e9750a6d7582f42546960f01784178
Oscar Leche <oleche@donomar.local>
2015-05-15 21:58:30 -0600

    Account updates

--- v1/cbfefd91213b0921e4156eb8215f0330/security/allow.php
+++ v1/cbfefd91213b0921e4156eb8215f0330/security/allow.php
@@ -1,9 +1,8 @@
 <?php
 
 class allow {
-	private static $PUBLISH = array('administrator', 'site-visitor', 'publish');
-    private static $COMMENT = array('administrator', 'site-visitor', 'publish');
-	private static $VOTE = array('administrator', 'site-visitor', 'publish');
+	private static $PUBLISH = array('module-owner', 'administrator', 'site-visitor', 'publish');
+  private static $COMMENT = array('administrator', 'site-visitor', 'publish');
 	private static $SEARCH = array('administrator', 'site-visitor');
 	private static $REMOVE = array('administrator', 'moderator');
 	private static $VALIDATE = array('administrator', 'moderator');
@@ -11,47 +10,43 @@ class allow {
 	private static $RESOLVE = array('administrator', 'owner');
 	private static $MANAGER = array('administrator', 'manager');
     private static $ADMINISTRATOR = array('administrator');
-	
+
 	public static function MANAGER(){
     	return self::$MANAGER;
 	}
-	
-    public static function ADMINISTRATOR(){
+
+  public static function ADMINISTRATOR(){
     	return self::$ADMINISTRATOR;
 	}
-    
+
 	public static function PUBLISH(){
 		return self::$PUBLISH;
 	}
-	
+
 	public static function RESOLVE(){
 		return self::$RESOLVE;
 	}
-	
+
 	public static function COMMENT(){
 		return self::$COMMENT;
-	}
-	
-	public static function VOTE(){
-		return self::$VOTE;
 	}
 	
 	public static function SEARCH(){
 		return self::$SEARCH;
 	}
-	
+
 	public static function REMOVE(){
 		return self::$REMOVE;
 	}
-	
+
 	public static function VALIDATE(){
 		return self::$VALIDATE;
 	}
-	
+
 	public static function MODERATE(){
 		return self::$MODERATE;
 	}
-	
+
 	public static function is_allowed($scopes, $allow){
 		$set = explode(',', $scopes);
 		$r_values = true;
@@ -61,7 +56,7 @@ class allow {
 		}
 		return $r_values;
 	}
-	
+
 	public static function denied($scopes){
 		$response = array();
 		$response['type'] = 'error';
