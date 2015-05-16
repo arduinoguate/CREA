dbda9fb44ef64a8dc14e53eefb6d24be5a448177
Oscar Leche <oleche@donomar.local>
2015-05-15 21:21:08 -0600

    Signup updates 0.12

--- js/contact_me.js
+++ js/contact_me.js
@@ -29,8 +29,8 @@ $(function() {
         cache: false,
         success: function(resp) {
           // Success message
-          console.log(resp);
-          json = resp;
+          console.log(resp.entidad);
+          json = JSON.parse(resp);
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
