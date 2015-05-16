576b236ca8576d7b6ba4e8f73ca876d7bb631a7a
Oscar Leche <oleche@donomar.local>
2015-05-15 21:28:04 -0600

    Login updates

--- login.php
+++ login.php
@@ -1,32 +1,35 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <title></title>
-        <meta charset="">
-        <link rel="stylesheet" media="screen" href="css/login.css">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-		<link rel="shortcut icon" type="img/png" href="img/favicon.png">
-        <script src="js/jquery-1.11.0.min.js" type="text/javascript"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="js/login.js"></script>
-    </head>
-    <body>
-    	<div class="container">
-		    <div class="login-container">
-	            <div id="output"></div>
-	            <div class="avatar"></div>
-	            <div class="form-box">
-	                <form id="loginfrm" action="" method="">
-	                    <input name="user" type="text" placeholder="username">
-	                    <div class="password">
-	                        <input type="password" name="passwordfield" id="passwordfield" placeholder="password">
-	                        <span class="glyphicon glyphicon-eye-open"></span>
-	                    </div>
-	                    <button class="btn btn-info btn-block login" type="submit">Login</button>
-	                </form>
-	            </div>
-	        </div>
-		</div>
-    </body>
+
+<head>
+  <title></title>
+  <meta charset="">
+  <link rel="stylesheet" media="screen" href="css/login.css">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="shortcut icon" type="img/png" href="img/favicon.png">
+  <script src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/login.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    <div class="login-container">
+      <div id="output"></div>
+      <div class="avatar"></div>
+      <div class="form-box">
+        <form id="loginfrm" action="" method="">
+          <input name="user" type="text" placeholder="username">
+          <div class="password">
+            <input type="password" name="passwordfield" id="passwordfield" placeholder="password">
+            <span class="glyphicon glyphicon-eye-open"></span>
+          </div>
+          <button class="btn btn-info btn-block login" type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
