93ea1f3d7567c4ef84a0fd2d450c8a02d9151f19
Oscar Leche <oleche@donomar.local>
2015-06-08 14:53:37 -0600

    Download site work 0.7

--- downloads.php
+++ downloads.php
@@ -77,11 +77,11 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
 		<script src="js/bootstrap.min.js"></script>
-		
+
 	<div class="row">
 		<div class="container">
 		  <h2>Portal de descargas</h2>
-			<<?php if (isset($_GET['plat'])): ?>
+			<?php if (isset($_GET['plat'])): ?>
 				<legend>Descargas para <?php echo $_GET['plat'] ?></legend>
 				<hr/>
 		  	<div class="row" id="files">
