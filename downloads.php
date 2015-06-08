e3199bf9cdd8c89d5e2f610640de0f1472ebd3d8
Oscar Leche <oleche@donomar.local>
2015-06-08 12:32:27 -0600

    download center init

---
+++ downloads.php
@@ -0,0 +1,145 @@
+<?php
+	session_start();
+
+	$api_ref = array();
+	$cmd_ref = array();
+
+	$files = glob('docs/lib/*.{yml}', GLOB_BRACE);
+	foreach($files as $file) {
+		if (trim($file) != "." || trim($file) != ".."){
+			$yaml = file_get_contents($file);
+
+			$parsed = yaml_parse($yaml);
+			$api_ref[] = $parsed;
+		}
+	}
+
+	$files = glob('docs/ws/*.{yml}', GLOB_BRACE);
+	foreach($files as $file) {
+		if (trim($file) != "." || trim($file) != ".."){
+			$yaml = file_get_contents($file);
+
+			$parsed = yaml_parse($yaml);
+			$cmd_ref[] = $parsed;
+		}
+	}
+
+	//print_r($parsed);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Crea - Documentacion</title>
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/docs.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+	<nav class="navbar navbar-default">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html"><span><img src="img/crea.png" width="25px"></span> Crea</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                       Documentación del API
+                    </li>
+										<?php if (isset($_SESSION['token'])): ?>
+
+											<li class="page-scroll">
+	                        <a href="dashboard.php">Dashboard</a>
+	                    </li>
+											<li class="page-scroll">
+	                        <a href="logout.php">Salir</a>
+	                    </li>
+
+										<?php endif; ?>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+	<div class="row">
+		<div class="container">
+		  <h2>Portal de descargas</h2>
+			<legend>Descargas para <?php echo $_GET['plat'] ?></legend>
+	  	<div class="row">
+
+	    </div>
+	  </div>
+  </div>
+
+
+
+
+
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-7853765-6', 'auto');
+	  ga('send', 'pageview');
+
+	  $(document).ready(function() {
+		    $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+		        e.preventDefault();
+		        $(this).siblings('a.active').removeClass("active");
+		        $(this).addClass("active");
+		        var index = $(this).index();
+		        $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+		        $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+		    });
+	  });
+
+	</script>
+</body>
