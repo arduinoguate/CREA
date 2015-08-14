d28aa91b9e44b833e9e87c0879eb2616039854cb
Oscar Leche <oleche@donomar.local>
2015-08-13 18:40:25 -0600

    Donate button added

--- dashboard.php
+++ dashboard.php
@@ -156,6 +156,17 @@
           </div>
 					<div class="row">
 						<div class="col-md-12">
+							<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+							<input type="hidden" name="cmd" value="_s-xclick">
+							<input type="hidden" name="hosted_button_id" value="D2BVD6DXXU4XW">
+							<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+							<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+							</form>
+							<p><center>Puedes apoyarnos realizando una donación</center></p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
 							<small>
 								Version: 0.1.<?php echo $version; ?>
 							</small>
