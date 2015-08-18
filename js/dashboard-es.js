3515241e14cf2599fad60b082c2efe0385b10c96
Oscar Leche <oleche@donomar.local>
2015-08-18 07:37:21 -0600

    Dashboard update 0.1

--- js/dashboard-es.js
+++ js/dashboard-es.js
@@ -366,8 +366,8 @@ $(document).ready(function() {
           info += 'Estado <b>' + item.estado + '</b><br/>Ultima Respuesta <b>' + item.last_response + '</b><br/>';
           info += '<small>Actualizado en: <b>' + item.updated_at + '</b></small></div>';
           info += '<div class="col-md-4"><div class="col-md-12"><button data-mod="' + item.id + '" class="btn-success btn-lg add_action">Agregar Acción</button></div>';
-          info += '<div class="col-md-12"><small><a href="'+item.tipo_modulo.url_libreria+'" target="_blank" class="text-left text-primary">Descargar Lib.</a></small></div>';
-          info += '<div class="col-md-12"><small><a href="#" class="eliminar_mod text-left" id="' + item.id + '">Eliminar</a></small></div></div>';
+          info += '<div class="col-md-12"><small><a href="'+item.tipo_modulo.url_libreria+'" target="_blank" class="text-right">Descargar Lib.</a></small></div>';
+          info += '<div class="col-md-12"><small><a href="#" class="eliminar_mod text-right text-danger" id="' + item.id + '">Eliminar</a></small></div></div>';
           info += '</div><hr/><p>Acciones</p>';
           info += '<div class="row" id="actions_int"></div>';
 
