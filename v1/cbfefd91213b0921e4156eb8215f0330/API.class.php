e7f4ec83d5cb2e21a349c6007cbefe01f828843a
Oscar Leche <oleche@donomar.local>
2015-08-27 15:23:12 -0600

    options 2

--- v1/cbfefd91213b0921e4156eb8215f0330/API.class.php
+++ v1/cbfefd91213b0921e4156eb8215f0330/API.class.php
@@ -109,7 +109,7 @@ abstract class API
       header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
 		  header("Access-Control-Allow-Origin: *");
 		  header("Access-Control-Allow-Headers: *");
-      header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS")
+      header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS");
 
   		foreach ($this->headers as $header) {
   			header($header);
