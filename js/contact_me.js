8cb3633ad80ed1b8aeb40fb2abd5972b55072fe6
Oscar Leche <oscar.leche@gmail.com>
2015-09-20 23:31:12 -0600

    Language 15

--- js/contact_me.js
+++ js/contact_me.js
@@ -6,54 +6,56 @@ $(function() {
       // additional error messages or events
     },
     submitSuccess: function($form, event) {
-      event.preventDefault(); // prevent default submit behaviour
-      // get values from FORM
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var user = $("input#user").val();
-      var pass = $("input#pass").val();
-      var firstName = name; // For Success/Failure Message
-      // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-      }
-      $.ajax({
-        url: "././post/signup.php",
-        type: "POST",
-        data: {
-          name: name,
-          email: email,
-          user: user,
-          pass: pass
-        },
-        cache: false,
-        success: function(resp) {
-          // Success message
-          json = JSON.parse(resp);
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Te has inscrito con éxito. Se ha enviado un correo a: "+json.entidad.email+"</strong>");
-          $('#success > .alert-success')
-            .append('</div>');
+      if ($form.attr("id") == 'contactForm'){
+        event.preventDefault(); // prevent default submit behaviour
+        // get values from FORM
+        var name = $("input#name").val();
+        var email = $("input#email").val();
+        var user = $("input#user").val();
+        var pass = $("input#pass").val();
+        var firstName = name; // For Success/Failure Message
+        // Check for white space in name for Success/Fail message
+        if (firstName.indexOf(' ') >= 0) {
+          firstName = name.split(' ').slice(0, -1).join(' ');
+        }
+        $.ajax({
+          url: "././post/signup.php",
+          type: "POST",
+          data: {
+            name: name,
+            email: email,
+            user: user,
+            pass: pass
+          },
+          cache: false,
+          success: function(resp) {
+            // Success message
+            json = JSON.parse(resp);
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append("</button>");
+            $('#success > .alert-success')
+              .append("<strong>Te has inscrito con éxito. Se ha enviado un correo a: "+json.entidad.email+"</strong>");
+            $('#success > .alert-success')
+              .append('</div>');
 
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        error: function(msj) {
-          console.log(msj);
-          json = JSON.parse(msj.responseText);
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append("<strong>No hemos logrado crear tu cuenta satisfactoriamente, "+json.message+"</strong>");
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-      })
+            //clear all fields
+            $('#contactForm').trigger("reset");
+          },
+          error: function(msj) {
+            console.log(msj);
+            json = JSON.parse(msj.responseText);
+            // Fail message
+            $('#success').html("<div class='alert alert-danger'>");
+            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append("</button>");
+            $('#success > .alert-danger').append("<strong>No hemos logrado crear tu cuenta satisfactoriamente, "+json.message+"</strong>");
+            $('#success > .alert-danger').append('</div>');
+            //clear all fields
+            $('#contactForm').trigger("reset");
+          },
+        })
+      }
     },
     filter: function() {
       return $(this).is(":visible");
