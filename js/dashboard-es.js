8c37857e0398d5b2bfd2857bd71cb282a084c0bf
Oscar Leche <oscar.leche@gmail.com>
2015-09-21 01:05:56 -0600

    Detele 9

--- js/dashboard-es.js
+++ js/dashboard-es.js
@@ -470,7 +470,7 @@ $(document).ready(function() {
       },
       error: function(jqXHR, textStatus, errorThrown) {
         if (jqXHR.status != '422') {
-          //window.location = "logout.php";
+          window.location = "logout.php";
         } else {
           if (jqXHR.responseJSON.code == 2)
             show_alert("Error de validacion de campos");
@@ -500,7 +500,7 @@ $(document).ready(function() {
       },
       error: function(jqXHR, textStatus, errorThrown) {
         if (jqXHR.status != '422') {
-          window.location = "logout.php";
+          //window.location = "logout.php";
         } else {
           if (jqXHR.responseJSON.code == 2)
             show_alert("Error de validacion de campos");
