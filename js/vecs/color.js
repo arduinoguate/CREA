5139b9fcc922eec246140aaf729f7532d465b565
Oscar Leche <oscar.leche@gmail.com>
2015-09-21 18:35:02 -0600

    Look nice 22

--- js/vecs/color.js
+++ js/vecs/color.js
@@ -1,17 +1,16 @@
 
 var colors = new Array(
-[62,35,255],
-  [60,255,60],
-  [255,35,98],
+  [62,35,255], //blue
+  [60,255,60], //green
+  [24,188,156], //redish
   [45,175,230],
-  [255,0,255],
   [255,128,0]);
 
 
-//  
+//
 
 var step = 0;
-//color table indices for: 
+//color table indices for:
 // current color left
 // next color left
 // current color right
@@ -23,9 +22,9 @@ var gradientSpeed = 0.002;
 
 function updateGradient()
 {
-  
+
   if ( $===undefined ) return;
-  
+
 var c0_0 = colors[colorIndices[0]];
 var c0_1 = colors[colorIndices[1]];
 var c1_0 = colors[colorIndices[2]];
@@ -45,19 +44,19 @@ var color2 = "rgb("+r2+","+g2+","+b2+")";
  $('.gradient').css({
    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
     background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-  
+
   step += gradientSpeed;
   if ( step >= 1 )
   {
     step %= 1;
     colorIndices[0] = colorIndices[1];
     colorIndices[2] = colorIndices[3];
-    
+
     //pick two new target color indices
     //do not pick the same as the current one
     colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
     colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-    
+
   }
 }
 
